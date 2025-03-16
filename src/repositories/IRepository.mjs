92c7ecf09a05baf47f33ab84e1f92590941b7e85
unknown <cancliniprogramming@gmail.com>
2025-03-16 19:45:44 -0300

    primer commit sprint2 tp3 final

---
+++ src/repositories/IRepository.mjs
@@ -0,0 +1,20 @@
+// Aqui se establece una interfaz donde define metodos CRUD estandar y sirve como contrato para asegurar que cualquier clase que implemente la interfaz cuente con estos metodos
+
+class IRepository {
+  obtenerPorId(id) {
+    throw new Error("Método 'obtenerPorId()' no implementado");
+  }
+  obtenerTodos() {
+    throw new Error("Método 'obtenerTodos()' no implementado");
+  }
+  buscarPorAtributo(atributo, valor) {
+    throw new Error("Método 'buscarPorAtributo()' no implementado");
+  }
+  obtenerMayoresDe30() {
+    throw new Error("Método 'obtenerMayoresDe30()' no implementado");
+  }
+}
+
+export default IRepository;
+
+// Esto proporciona una abstraccion de los metodos CRUD que deben ser implementado por cualquier repositorio. Esto asegura que todas las clases de repositorio mantengan consistencia en sus metodos, mejorando la mantenibilidad y facilitando cambios futuros en la implementación.
---
+++ src/repositories/SuperHeroRepository.mjs
@@ -0,0 +1,21 @@
+import SuperHero from '../models/SuperHero.mjs';
+import IRepository from './IRepository.mjs';
+
+class SuperHeroRepository extends IRepository {
+  async obtenerPorId(id) {
+    return await SuperHero.findById(id);
+  }
+  async obtenerTodos() {
+    return await SuperHero.find({});
+  }
+  async buscarPorAtributo(atributo, valor) {
+    return await SuperHero.find({ [atributo]: valor });
+  }
+  async obtenerMayoresDe30() {
+    return await SuperHero.find({ edad: { $gt: 30 } }); //$gt: "greater than" (mayor que)
+  }
+}
+
+export default new SuperHeroRepository();
+
+// Aqui se implementan los metodos definidos en la interfaz, interactuando directamente con MongoDB a traves de Mongoose para realizar operaciones de datos. La centralizacion de estas operaciones en el repositorio mejora la organizacion y garantiza que el acceso a los datos se realice de manera controlada y uniforme.
